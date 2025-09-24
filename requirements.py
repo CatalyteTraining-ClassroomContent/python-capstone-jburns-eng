8c3f398cec09ed39ed42372e1a46e81ba68dfe37
James Burns <jburns@catalyte.io>
2025-09-24 12:04:34 -0500

    plz forgive us for not committing

--- requirements.py
+++ requirements.py
@@ -4,7 +4,7 @@ quiz_submmission1 = {
     "quiz_score": "95",
     "student_id": "100",
     "student_name": "Jessica Alba",
-    "submission_date": "09-22-2025",
+    "submission_date": "9-22-2025",
 }
 
 quiz_submission2 = {
@@ -13,7 +13,7 @@ quiz_submission2 = {
     "quiz_score": "77",
     "student_id": "101",
     "student_name": "Mark Walhberg",
-    "submission_date": "09-22-2025",
+    "submission_date": "9-22-2025",
 }
 
 quiz_submission3 = {
@@ -22,7 +22,7 @@ quiz_submission3 = {
     "quiz_score": "100",
     "student_id": "102",
     "student_name": "Tony Stark",
-    "submission_date": "09-23-2025",
+    "submission_date": "9-23-2025",
 }
 
 quiz_submission4 = {
@@ -31,7 +31,7 @@ quiz_submission4 = {
     "quiz_score": "88",
     "student_id": "101",
     "student_name": "Mark Walhberg",
-    "submission_date": "09-22-2025",
+    "submission_date": "9-22-2025",
 }
 
 quiz_submission5 = {
@@ -40,7 +40,7 @@ quiz_submission5 = {
     "quiz_score": "74",
     "student_id": "100",
     "student_name": "Jessica Alba",
-    "submission_date": "09-22-2025",
+    "submission_date": "9-22-2025",
 }
 
 quiz_submission6 = {
@@ -49,7 +49,7 @@ quiz_submission6 = {
     "quiz_score": "77",
     "student_id": "102",
     "student_name": "Tony Stark",
-    "submission_date": "09-22-2025",
+    "submission_date": "9-22-2025",
 }
 
 quiz_submission7 = {
@@ -58,7 +58,7 @@ quiz_submission7 = {
     "quiz_score": "99",
     "student_id": "102",
     "student_name": "Tony Stark",
-    "submission_date": "09-22-2025",
+    "submission_date": "9-22-2025",
 }
 
 quiz_submission8 = {
@@ -67,7 +67,7 @@ quiz_submission8 = {
     "quiz_score": "83",
     "studend_id": "100",
     "student_name": "Jessica Alba",
-    "submission_date": "09-23-2025",
+    "submission_date": "9-23-2025",
 }
 
 quiz_submission9 = {
@@ -76,7 +76,7 @@ quiz_submission9 = {
     "quiz_score": "87",
     "student_id": "101",
     "student_name": "Mark Walhberg",
-    "submission_date": "09-23-2025",
+    "submission_date": "9-23-2025",
 }
 
 
@@ -87,7 +87,7 @@ submissions = [
         "quiz_score": "74",
         "student_id": "100",
         "student_name": "Jessica Alba",
-        "submission_date": "09-22-2025",
+        "submission_date": "9-22-2025",
     },
     {
         "quiz_name": "Calculating Fractions",
@@ -95,7 +95,7 @@ submissions = [
         "quiz_score": "77",
         "student_id": "101",
         "student_name": "Mark Walhberg",
-        "submission_date": "09-22-2025",
+        "submission_date": "9-22-2025",
     },
     {
         "quiz_name": "Supply and Demand",
@@ -103,7 +103,7 @@ submissions = [
         "quiz_score": "100",
         "student_id": "102",
         "student_name": "Tony Stark",
-        "submission_date": "09-23-2025",
+        "submission_date": "9-23-2025",
     },
     {
         "quiz_name": "Fruit Transition",
@@ -111,7 +111,7 @@ submissions = [
         "quiz_score": "88",
         "student_id": "101",
         "student_name": "Mark Walhberg",
-        "submission_date": "09-22-2025",
+        "submission_date": "9-22-2025",
     },
     {
         "quiz_name": "Calculating Fractions",
@@ -119,7 +119,7 @@ submissions = [
         "quiz_score": "74",
         "student_id": "100",
         "student_name": "Jessica Alba",
-        "submission_date": "09-22-2025",
+        "submission_date": "9-22-2025",
     },
     {
         "quiz_name": "Fruit Transition",
@@ -127,7 +127,7 @@ submissions = [
         "quiz_score": "77",
         "student_id": "102",
         "student_name": "Tony Stark",
-        "submission_date": "09-22-2025",
+        "submission_date": "9-22-2025",
     },
     {
         "quiz_name": "Calculating Fractions",
@@ -135,7 +135,7 @@ submissions = [
         "quiz_score": "99",
         "student_id": "102",
         "student_name": "Tony Stark",
-        "submission_date": "09-22-2025",
+        "submission_date": "9-22-2025",
     },
     {
         "quiz_name": "Supply and Demand",
@@ -143,7 +143,7 @@ submissions = [
         "quiz_score": "83",
         "studend_id": "100",
         "student_name": "Jessica Alba",
-        "submission_date": "09-23-2025",
+        "submission_date": "9-23-2025",
     },
     {
         "quiz_name": "Supply and Demand",
@@ -151,7 +151,7 @@ submissions = [
         "quiz_score": "87",
         "student_id": "101",
         "student_name": "Mark Walhberg",
-        "submission_date": "09-23-2025",
+        "submission_date": "9-23-2025",
     },
 ]
 
@@ -165,11 +165,11 @@ def filter_by_date(submissions, date):
 
 
 def filter_by_student_id(submissions, student_id):
-    filtered_submissions = []
-    for submission in submissions:
-        if submission["student_id"] == student_id:
-            filtered_submissions.append(submission)
-    return filtered_submissions
+    return [
+        submissions
+        for submissions in submissions
+        if submissions.get("studentId") == student_id
+    ]
 
 
 def find_unsubmitted_quizzes(submissions, student_id):
@@ -186,13 +186,20 @@ def get_average_score(submissions):
 
 
 def get_average_score_by_module(submissions, module):
-    filtered_submissions = filter_by_module(submissions, module)
+    filtered_submissions = sum(submissions, module)
     total = sum(submission["quiz_score"] for submission in filtered_submissions)
     return total / len(filtered_submissions)
 
 
 print(submissions)
-print(filter_by_student_id)
+date_filtered = filter_by_date(submissions, "9-22-2025")
+date_filtered = filter_by_date(submissions, "9-23-2025")
+print(date_filtered, "9-22-2025")
+print(date_filtered, "9-23-2025")
+id_filtered = filter_by_student_id(submissions, "100")
+print(id_filtered, "Jessica Alba")
+unsubmitted_quiz = find_unsubmitted_quizzes(submissions)
+print(unsubmitted_quiz)
 print(find_unsubmitted_quizzes)
 print(get_average_score)
 print(get_average_score_by_module)
