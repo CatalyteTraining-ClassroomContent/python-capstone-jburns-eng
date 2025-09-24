aef49ae6bedf15ff42c56b184615fd545c0929ec
James Burns <jburns@catalyte.io>
2025-09-24 09:53:04 -0500

    listing submissions

---
+++ requirements.py
@@ -0,0 +1,26 @@
+quiz_submmission1 = {
+    "quiz_name": "Fruit Transition",
+    "quiz_module": "Art",
+    "quiz_score": "95",
+    "student_id": "100",
+    "student_name": "Jessica Alba",
+    "submission_date": "09-22-2025",
+}
+
+quiz_submission2 = {
+    "quiz_name": "Calculating Fractions",
+    "quiz_module": "Algebra",
+    "quiz_score": "77",
+    "student_id": "101",
+    "student_name": "Mark Walhberg",
+    "submission_date": "09-22-2025",
+}
+
+quiz_submission3 = {
+    "quiz_name": "Supply and Demand",
+    "quiz_module": "Economics",
+    "quiz_score": "100",
+    "student_id": "102",
+    "student_name": "Tony Stark",
+    "submission_date": "09-23-2025",
+}
