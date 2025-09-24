64d3fcd18d9f2652408eb3813d7cf0ef4b823ba6
James Burns <jburns@catalyte.io>
2025-09-24 10:21:35 -0500

    submissions

--- requirements.py
+++ requirements.py
@@ -24,3 +24,117 @@ quiz_submission3 = {
     "student_name": "Tony Stark",
     "submission_date": "09-23-2025",
 }
+
+quiz_submission4 = {
+    "quiz_name": "Fruit Transition",
+    "quiz_module": "Art",
+    "quiz_score": "88",
+    "student_id": "101",
+    "student_name": "Mark Walhberg",
+    "submission_date": "09-22-2025",
+
+}
+
+quiz_submission5 = {
+    "quiz_name": "Calculating Fractions",
+    "quiz_module": "Algebra",
+    "quiz_score": "74",
+    "student_id": "100",
+    "student_name": "Jessica Alba",
+    "submission_date": "09-22-2025"
+}
+
+quiz_submission6 = {
+    "quiz_name": "Fruit Transition",
+    "quiz_module": "Art",
+    "quiz_score": "77",
+    "student_id": "102",
+    "student_name": "Tony Stark",
+    "submission_date": "09-22-2025",
+}
+
+quiz_submission7 = {
+    "quiz_name": "Calculating Fractions",
+    "quiz_module": "Algebra",
+    "quiz_score": "99",
+    "student_id": "102",
+    "student_name": "Tony Stark",
+    "submission_date": "09-22-2025"
+}
+
+quiz_submission8 = {
+    "quiz_name": "Supply and Demand",
+    "quiz_module": "Economics",
+    "quiz_score": "83",
+    "studend_id": "100",
+    "student_name": "Jessica Alba",
+    "submission_date": "09-23-2025",
+}
+
+quiz_submission9 = {
+    "quiz_name": "Supply and Demand",
+    "quiz_module": "Economics",
+    "quiz_score": "87",
+    "student_id": "101",
+    "student_name": "Mark Walhberg",
+    "submission_date": "09-23-2025",
+}
+
+
+submissions = [
+    {"quiz_name": "Calculating Fractions",
+    "quiz_module": "Algebra",
+    "quiz_score": "74",
+    "student_id": "100",
+    "student_name": "Jessica Alba",
+    "submission_date": "09-22-2025"}
+    {"quiz_name": "Calculating Fractions",
+    "quiz_module": "Algebra",
+    "quiz_score": "77",
+    "student_id": "101",
+    "student_name": "Mark Walhberg",
+    "submission_date": "09-22-2025",}
+    {"quiz_name": "Supply and Demand",
+    "quiz_module": "Economics",
+    "quiz_score": "100",
+    "student_id": "102",
+    "student_name": "Tony Stark",
+    "submission_date": "09-23-2025",}
+    {"quiz_name": "Fruit Transition",
+    "quiz_module": "Art",
+    "quiz_score": "88",
+    "student_id": "101",
+    "student_name": "Mark Walhberg",
+    "submission_date": "09-22-2025",}
+    {"quiz_name": "Calculating Fractions",
+    "quiz_module": "Algebra",
+    "quiz_score": "74",
+    "student_id": "100",
+    "student_name": "Jessica Alba",
+    "submission_date": "09-22-2025"}
+    {"quiz_name": "Fruit Transition",
+    "quiz_module": "Art",
+    "quiz_score": "77",
+    "student_id": "102",
+    "student_name": "Tony Stark",
+    "submission_date": "09-22-2025",}
+    {"quiz_name": "Calculating Fractions",
+    "quiz_module": "Algebra",
+    "quiz_score": "99",
+    "student_id": "102",
+    "student_name": "Tony Stark",
+    "submission_date": "09-22-2025"}
+    {"quiz_name": "Supply and Demand",
+    "quiz_module": "Economics",
+    "quiz_score": "83",
+    "studend_id": "100",
+    "student_name": "Jessica Alba",
+    "submission_date": "09-23-2025",}
+    {"quiz_name": "Supply and Demand",
+    "quiz_module": "Economics",
+    "quiz_score": "87",
+    "student_id": "101",
+    "student_name": "Mark Walhberg",
+    "submission_date": "09-23-2025",}
+]
+
